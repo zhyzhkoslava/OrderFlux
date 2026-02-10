370316a835112cce39d7726b0761ef37acc2c9b3
zhyzhkoslava <zhizhkoslava@gmail.com>
2026-02-10 10:21:35 +0200

    set-up

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'));
+Route::get('/', fn (): Factory|View => view('welcome'));
